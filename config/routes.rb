e626ec92794c68879bda93e7702cbda95caf59ab
takazawa-hokuto <ace2015@i.softbank.jp>
2021-02-20 00:07:34 +0000

    rails05

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
   
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
 
 
